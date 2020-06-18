489f07ee3348f9e4a38ca6787aad19bcd82bc124
osama_shiraz <osama.shiraz@o3interfaces.com>
2020-06-18 13:55:26 +0500

    Initial Commit

---
+++ HayesMuslimCentre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HayesMuslimCentre
+//
+//  Created by OBS on 18/06/2020.
+//  Copyright © 2020 OsamaShiraz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
